584bd9a3fe6f614717466085991b5660312069da
Yojana-a <yojana.luitel20@gmail.com>
2026-02-20 15:46:40 -0600

    made changes to main class

--- display.java
+++
@@ -1,9 +0,0 @@
-public static void printInventory(String[] names, double[] prices, int[] stocks){
-    for(int i=0; i<names.length; i++){
-        if(names[i]!=null){
-            System.out.println("Item: "+ names[i]);
-            System.out.println("Price: "+prices[i]);
-            System.out.println("Stock "+stocks[i]);
-        }
-    }
-}
